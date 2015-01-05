8a00bfcc958911086c8603a992587174128f02a3
lys2907 <teqwin@Teqwins-MacBook-Pro.local>
2015-01-05 14:12:55 +0800

    Revert f22b831..4130b10
    This rolls back to commit f22b8314bd01d45bd0972490647c8e382c61a5ce.

--- TEQWIN_PROJECT/Tattoo_Detail_ViewController.m
+++ TEQWIN_PROJECT/Tattoo_Detail_ViewController.m
@@ -65,7 +65,15 @@
     
     CGSize maxSize = CGSizeMake(320, 410);
     CGRect labrect = [lorum boundingRectWithSize:maxSize options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:_desc.font} context:Nil];
-       NSLog(@"1%f",[UIScreen mainScreen].bounds.size.height);
+    if ([UIScreen mainScreen].bounds.size.height ==480) {
+        [self.scrollView setContentSize:CGSizeMake(320, 580+labrect.size.height+200)];
+    }
+    else
+    {
+        [self.scrollView setContentSize:CGSizeMake(320, [UIScreen mainScreen].bounds.size.height+labrect.size.height+100)];
+    
+    }
+    NSLog(@"1%f",[UIScreen mainScreen].bounds.size.height);
     NSLog(@"2%f",labrect.size.height);
     _desc.text = lorum;
     //for use UITextView you should comment the line under
